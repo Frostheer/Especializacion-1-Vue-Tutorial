f1090c286ae26b1b725e444351605b26b87deac5
Esteban Candia <E.candxa@gmail.com>
2024-05-03 15:10:09 -0400

    First commit

---
+++ vuetutorial/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <MiComponente/>
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+import MiComponente from "@/components/MiComponente.vue";
+
+export default defineComponent({
+  components: {MiComponente}
+})
+</script>
+
+<style scoped>
+
+</style>
